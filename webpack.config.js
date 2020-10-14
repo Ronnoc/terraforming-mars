a74ece98359d1f99b9cfacff2612664ac067e8cc
Ronnoc <kybconnor@126.com>
2020-10-14 14:38:18 +0800

    fix webpack to bafolts

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,19 @@
-"use strict"
+'use strict'
 
 const CompressionPlugin = require("compression-webpack-plugin");
 
 module.exports = {
   devtool: "source-map",
-  mode: "production",
+  mode: 'production',
   entry: [
-    "./dist/script.js"
+    './dist/script.js'
   ],
   plugins: [new CompressionPlugin()],
   resolve: {
     alias: {
-        "vue$": "vue/dist/vue.esm.js" // 'vue/dist/vue.common.js' for webpack 1
+        'vue$': 'vue/dist/vue.esm.js' // 'vue/dist/vue.common.js' for webpack 1
     }
   },
-  optimization: {
-    splitChunks: {
-    },
-  },
-  plugins: [
-    new CompressionPlugin({
-          filename: "[path].gz[query]",
-          algorithm: "gzip",
-          test: /\.js$|\.js.map$|\.css$|\.html$|\.eot?.+$|\.ttf?.+$|\.woff?.+$|\.svg?.+$/,
-          threshold: 10240,
-          minRatio: 0.8
-    }),
-  ],
   stats: {
     warnings: false
   }
